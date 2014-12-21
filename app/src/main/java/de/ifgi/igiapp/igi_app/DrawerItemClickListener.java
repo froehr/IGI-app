ee1fe28dbd3853095c45e947d35e36abbbd7e97b
ReneUnrau <renegolas@web.de>
2014-12-21 16:18:37 +0100

    First work for story-list

--- app/src/main/java/de/ifgi/igiapp/igi_app/DrawerItemClickListener.java
+++ app/src/main/java/de/ifgi/igiapp/igi_app/DrawerItemClickListener.java
@@ -1,5 +1,6 @@
 package de.ifgi.igiapp.igi_app;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -10,36 +11,13 @@ import android.widget.ListView;
 public class DrawerItemClickListener implements ListView.OnItemClickListener {
     @Override
     public void onItemClick(AdapterView parent, View view, int position, long id) {
-        //selectItem(position);
-        System.out.println("Drawer item clicked");
+        selectItem(position, view);
     }
 
-
-    /** Swaps fragments in the main content view */
-    /*
-    private void selectItem(int position) {
-        // Create a new fragment and specify the planet to show based on position
-        Fragment fragment = new PlanetFragment();
-        Bundle args = new Bundle();
-        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
-        fragment.setArguments(args);
-
-        // Insert the fragment by replacing any existing fragment
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.content_frame, fragment)
-                .commit();
-
-        // Highlight the selected item, update the title, and close the drawer
-        mDrawerList.setItemChecked(position, true);
-        setTitle(mPlanetTitles[position]);
-        mDrawerLayout.closeDrawer(mDrawerList);
+    private void selectItem(int position, View view) {
+        if(position == 1){
+            Intent intent = new Intent(view.getContext(), StoryListActivity.class);
+            view.getContext().startActivity(intent);
+        }
     }
-
-    @Override
-    public void setTitle(CharSequence title) {
-        mTitle = title;
-        getActionBar().setTitle(mTitle);
-    }
-    */
 }
