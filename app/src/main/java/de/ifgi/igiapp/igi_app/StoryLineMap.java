700a4a307beb4d7bdfb4ee12039101eab5c0f6cf
ReneUnrau <renegolas@web.de>
2015-01-17 12:41:52 +0100

    Starting location listener

--- app/src/main/java/de/ifgi/igiapp/igi_app/StoryLineMap.java
+++ app/src/main/java/de/ifgi/igiapp/igi_app/StoryLineMap.java
@@ -1,14 +1,25 @@
 package de.ifgi.igiapp.igi_app;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
@@ -19,11 +30,17 @@ import de.ifgi.igiapp.igi_app.MongoDB.Poi;
 import de.ifgi.igiapp.igi_app.MongoDB.Story;
 import de.ifgi.igiapp.igi_app.MongoDB.StoryElement;
 
-public class StoryLineMap extends FragmentActivity {
+public class StoryLineMap extends FragmentActivity implements GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener{
 
     private GoogleMap mMap;
     private String storyId;
     private DatabaseHandler databaseHandler;
+    private LocationClient mLocationClient;
+    private LocationManager mLocationManager;
+    private LocationListener mLocationListener;
+    private boolean connected = false;
+    private boolean startIfConnected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +53,29 @@ public class StoryLineMap extends FragmentActivity {
         databaseHandler = application.getGlobalDatabaseHandler();
 
         setUpMapIfNeeded();
+
+        // location manager for updating position and calculating distances
+        mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // for initial position
+        mLocationClient = new LocationClient(this, this,this);
+        mLocationClient.connect();
+
+        final Button button = (Button) findViewById(R.id.startStoryButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // start as soon as connection is established
+                startIfConnected = true;
+
+                // if not yet connect inform user
+                if (!connected) {
+                    Toast.makeText(getApplicationContext(), "Waiting for GPS", Toast.LENGTH_LONG).show();
+                } else {
+                    startStory();
+                }
+            }
+        });
     }
 
     @Override
@@ -98,5 +138,55 @@ public class StoryLineMap extends FragmentActivity {
         int padding = 100;
         CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
         mMap.animateCamera(cu);
+    }
+
+    public void startStory(){
+        Location mCurrentLocation = mLocationClient.getLastLocation();
+        centerMapOnLocation(mCurrentLocation);
+        startLocationListener();
+    }
+
+    private void startLocationListener(){
+        LocationListener locationListener = new LocationListener(){
+            public void onLocationChanged (Location location) {
+                Toast.makeText(getApplicationContext(), "Received new position", Toast.LENGTH_SHORT).show();
+                centerMapOnLocation(location);
+            }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+            @Override
+            public void onProviderEnabled(String provider) {}
+            @Override
+            public void onProviderDisabled(String provider) {}
+        };
+
+        // register listener
+        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0,locationListener);
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+    }
+
+    public void centerMapOnLocation(Location location){
+        LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 15));
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        connected = true;
+        Toast.makeText(getApplicationContext(), "GPS established", Toast.LENGTH_LONG).show();
+
+        if(startIfConnected){
+            startStory();
+        }
+    }
+
+    @Override
+    public void onDisconnected() {
+        Toast.makeText(getApplicationContext(), "GPS is not enabled", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(getApplicationContext(), "GPS is not enabled", Toast.LENGTH_LONG).show();
     }
 }
