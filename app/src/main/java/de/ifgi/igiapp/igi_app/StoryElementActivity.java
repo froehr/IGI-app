0984732e861c87664cfa2bbdb021f2a30a9b0af8
Daniel <daniel.sawatzky@uni-muenster.de>
2015-01-28 13:42:33 +0100

    test

--- app/src/main/java/de/ifgi/igiapp/igi_app/StoryElementActivity.java
+++ app/src/main/java/de/ifgi/igiapp/igi_app/StoryElementActivity.java
@@ -35,6 +35,7 @@ public class StoryElementActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.story_element, menu);
         return true;
